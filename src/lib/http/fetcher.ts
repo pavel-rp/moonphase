4dcd284b9fcc24a10918d3686e04acc32cccf742
recky-ru <recky.ru@gmail.com>
2025-09-02 19:24:43 +0300

    Add timeout rejection to withTimeout function in fetcher.ts

--- src/lib/http/fetcher.ts
+++ src/lib/http/fetcher.ts
@@ -2,6 +2,7 @@ import { setTimeout as sleep } from 'node:timers/promises';
 
 export async function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
   const timeout = new Promise<never>((_, reject) => {
+    setTimeout(() => {
       reject(new Error(`Timeout after ${ms}ms`));
     }, ms);
   });
